3120555e6af08021396e2f5ae08dc4f5f3fe2532
mabbason <miles.abbason@gmail.com>
2022-05-05 15:35:06 -0400

    rollup commit on various projects

---
+++ bmi/bmiCalculator.ts
@@ -0,0 +1,34 @@
+// type Operation = 'multiply' | 'add' | 'divide';
+
+// const calculator = (a: number, b: number, op: Operation): number => {
+//   switch(op) {
+//     case "multiply":
+//       return a * b
+//     case "add":
+//       return a + b
+//     case "divide":
+//       if (b === 0) throw new Error("Can't divide by 0!")
+//       return a / b
+//     default:
+//       throw new Error("Invalid operation")
+//   }
+// }
+
+const calculateBmi = (height: number, weight: number): string => {
+  
+  return "test"
+}
+
+// try {
+//   console.log(calculator(1, 5 , 'divide'));
+// } catch (error: unknown) {
+//   let errorMessage = 'Something went wrong.'
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message;
+//   }
+//   console.log(errorMessage);
+// }
+
+// calculator(1, 2, "add")
+
+console.log(calculateBmi(180, 74))
